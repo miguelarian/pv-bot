b073cf75a580df21513425feeb46a4ece6c84db9
miguel <miguel>
2018-10-02 22:47:20 +0200

    Added model

--- src/Action.ts
+++ src/Action.ts
@@ -1,3 +1,13 @@
+const ActionsCost: { [userId: string]: number } = {
+    attack: 0,
+    defend: 4,
+    insult: 2,
+    rest: 5,
+    skip: 0,
+    take: 3,
+    use: 3
+}
+
 class Action {
     action: string
     source: string
