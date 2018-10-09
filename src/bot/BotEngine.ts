7564d308ad9ca6061cea4afca9f5cfa1b337f6a9
Miguel Vela Romera <miguelvr88@gmail.com>
2018-10-09 21:25:52 +0200

    Modified eslint changes

--- src/bot/BotEngine.ts
+++ src/bot/BotEngine.ts
@@ -1,25 +1,25 @@
-import { Action } from '../models/Action';
-import { State } from '../models/State';
-import { StrategyAI } from './strategies/StrategyAI';
+import { Action } from '../models/Action'
+import { State } from '../models/State'
+import { StrategyAI } from './strategies/StrategyAI'
 
 export class BotEngine {
     private ai: StrategyAI
-    constructor () {
-        this.ai = new StrategyAI();
+    constructor() {
+        this.ai = new StrategyAI()
     }
-    process (state: State) : Action[] {
-        console.log(`Turn ${state.turns.current}: Processing state...`);
+    process(state: State): Action[] {
+        console.log(`Turn ${state.turns.current}: Processing state...`)
 
-        const warriorsStrategies = this.ai.getStrategy(state);
-        console.log(`Warriors strategies: ${warriorsStrategies.toString()}`);
+        const warriorsStrategies = this.ai.getStrategy(state)
+        console.log(`Warriors strategies: ${warriorsStrategies.toString()}`)
 
-        let actions = new Array<Action>();
+        let actions = new Array<Action>()
         warriorsStrategies.forEach(strategy => {
-            return actions.push(...strategy.getActions());
-        });
+            return actions.push(...strategy.getActions())
+        })
 
-        console.log(`Actions returned: ${actions.map(i => i.action).join(',')}`);
+        console.log(`Actions returned: ${actions.map(i => i.action).join(',')}`)
 
-        return actions;
+        return actions
     }
 }
